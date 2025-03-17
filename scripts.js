0f8c9ac153dd80caf7ded920703ac747f0d29fe1
Kaushal Patil <124388138+mraaglave@users.noreply.github.com>
2025-03-17 19:55:49 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,76 @@
+const syllabusLinks = {
+    "1": {
+        "Computer Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view",
+        "Civil Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view",
+        "Electrical Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view",
+        "Electronics Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view",
+        "Mechanical Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view",
+        "Chemical Engineering": "https://drive.google.com/file/d/1RVJoAajSEyVNCAjE8ZhomFMPEAIQf9SW/view"
+    },
+    "2": {
+        "Computer Engineering": "https://drive.google.com/file/d/1dsbqGU47mBUcCmVHe1thBxEGPEyYOuSp/view?usp=sharing",
+        "Civil Engineering": "https://drive.google.com/file/d/1G_Rhv0ZdS5NLGAh17VYfOFiGr7snt7XW/view?usp=sharing",
+        "Electrical Engineering": "https://drive.google.com/file/d/1WplDBhkOghhssaiQdVIdcfOJccx7A8tw/view?usp=sharing",
+        "Electronics Engineering": "https://drive.google.com/file/d/1ZQViOohIEKO4Cu7EN7YZAWoe0ocDneyw/view?usp=sharing",
+        "Mechanical Engineering": "https://drive.google.com/file/d/1za1omnYKr4v0H7n5y2qUoCnC64EgLb76/view?usp=sharing",
+        "Chemical Engineering": "https://drive.google.com/file/d/1l41xPhiPGlXV8OR_KkAB72iEUfaCz_bY/view?usp=sharing"
+    },
+    "3": {
+        "Computer Engineering": "https://drive.google.com/uc?export=download&id=1cx-gjD5zVLLdY8Smoej-wZAoehhJMuL2",
+        "Civil Engineering": "https://drive.google.com/uc?export=download&id=1MsSFBnUd6eca5i2PsfUnChL00TC7KM_s",
+        "Electrical Engineering": "https://drive.google.com/uc?export=download&id=1YE-zTnm8lJbgiqNLXHin2OmxEg4_GWYs",
+        "Electronics Engineering": "https://drive.google.com/file/d/1snUbk_gUL55PsVZsued9xr13WD0m3N_I/view?usp=sharing",
+        "Mechanical Engineering": "https://drive.google.com/uc?export=download&id=1nHpgiCYXoPJ51iYhBwJW7EI7sqPoWEB4",
+        "Information Technology Engineering": "https://drive.google.com/uc?export=download&id=1h9XhmgzBxE2JDX4l4vdSy25kPhaxnEJX"
+    },
+    "4": {
+        "Computer Engineering": "https://drive.google.com/uc?export=download&id=1YdbeUf4yn1kyhCLND1mLQd-7YnDesZro",
+        "Civil Engineering": "https://drive.google.com/uc?export=download&id=10FK94HbzvKF_35zQ4cmczeJEASw6C3uU",
+        "Electrical Engineering": "https://drive.google.com/uc?export=download&id=1Pd-aOMw4OVapii2H_9mjNiDEJT4z_Ltj",
+        "Electronics Engineering": "https://drive.google.com/uc?export=download&id=1t9Gqz3SJ4GyCmNvQHeOERpXKKNBMaXC0",
+        "Mechanical Engineering": "https://drive.google.com/uc?export=download&id=1JTf9uQYHr6SuJv1y7vfxyvlAs57zrqLt",
+        "Information Technology Engineering": "https://drive.google.com/uc?export=download&id=1VDhw6cLlUOHDkHKYN_jz8NsaJm4nOS38",
+        "Chemical Engineering": "https://drive.google.com/uc?export=download&id=1zP5nP_KBeYXdYHCY_e3BfpaI_WgihgQC"
+    },
+    "5": {
+        "Computer Engineering": "https://drive.google.com/uc?export=download&id=1TUoCx93zGKCUcCd7cWAsysJASDBfFQ7z",
+        "Civil Engineering": "https://drive.google.com/uc?export=download&id=1p59esJdSqRZ36cHIptsZE1csqmaHnuvA",
+        "Electrical Engineering": "https://drive.google.com/uc?export=download&id=1jz3eIXhQ4AoZxjZrStf80lh4G0YRyrCr",
+        "Electronics Engineering": "https://drive.google.com/uc?export=download&id=1maQGVsjKo6uMtZyERbJTkn7WHzSKCQtn",
+        "Mechanical Engineering": "https://drive.google.com/uc?export=download&id=1xr7qKPiC2DZa_WMFo-dTbhmVD4d-dwWO",
+        "Information Technology Engineering": "https://drive.google.com/uc?export=download&id=1MVpa5t-x2HT4XekEJxvcl_P_on63HphN",
+        "Chemical Engineering": "https://drive.google.com/uc?export=download&id=1R30OmgF5DC8TQWF4K0j6qLkbkBNPVN0F"
+    },
+    "6": {
+        "Computer Engineering": "https://drive.google.com/uc?export=download&id=1P9OZHhixe5ywcGIt2d0nM3LQyIzlDEXz",
+        "Civil Engineering": "https://drive.google.com/uc?export=download&id=1-LEI9zaW8_Ih_z63RSR2Z8rW_AC2C2CW",
+        "Electrical Engineering": "https://drive.google.com/uc?export=download&id=19qa_s8ffwnpFBnY6KLqPwezGXzTNf-vA",
+        "Electronics Engineering": "https://drive.google.com/uc?export=download&id=1nrs_Hyp03tN3QlhHDcQUn4UM_Ng7SHpY",
+        "Mechanical Engineering": "https://drive.google.com/uc?export=download&id=1neZkHMZyp8_Ri2vk8GDY8A1-icTdm3xb",
+        "Information Technology Engineering": "https://drive.google.com/uc?export=download&id=1f_M0cKwtA_RSrRXul_kAXom1BUuLgBuD",
+        "Chemical Engineering": "https://drive.google.com/uc?export=download&id=1svqJ7tN5Ra499xnbtnYEsMKSI0OrKOdD"
+    }
+};
+
+document.getElementById('branch').addEventListener('change', updateDownloadLink);
+document.getElementById('semester').addEventListener('change', updateDownloadLink);
+
+function updateDownloadLink() {
+    const branch = document.getElementById('branch').value;
+    const semester = document.getElementById('semester').value;
+    const downloadButton = document.getElementById('download-button');
+
+    if (branch && semester) {
+        const link = syllabusLinks[semester][branch];
+        if (link) {
+            downloadButton.disabled = false;
+            downloadButton.onclick = () => window.open(link, '_blank');
+        } else {
+            downloadButton.disabled = true;
+            downloadButton.onclick = null;
+        }
+    } else {
+        downloadButton.disabled = true;
+        downloadButton.onclick = null;
+    }
+}
